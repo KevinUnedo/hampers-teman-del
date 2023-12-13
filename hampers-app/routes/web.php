a8febb92da6d82d40effd6c6c9ed32fbee453753
KendoSir <kevinunedo14@gmail.com>
2023-12-13 10:52:14 +0700

    Creating the Homepage

--- hampers-app/routes/web.php
+++ hampers-app/routes/web.php
@@ -13,15 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
-
-
 Route::get('/login', function () {
     return view('login');
 });
 
 Route::get('/category', function () {
     return view('category');
+});
+
+Route::get('/', function () {
+    return view('home');
 });
